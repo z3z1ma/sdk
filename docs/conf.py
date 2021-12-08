be9b8d492843eaf0ab20c0554c35a2a7c9cf4d6c
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-12-08 02:20:44 -0600

    docs: Link to source code for classes and methods

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ extensions = [
     "sphinx_rtd_theme",
     "sphinx_copybutton",
     "myst_parser",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
